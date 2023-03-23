e26f8980666d0c48b9b2411e7311f87b5d97c9d5
HETPAHHEP <putinisathief@pm.me>
2023-03-23 19:38:04 +0300

    add context processor for footer.html with current year

--- wisejournal/posts/views.py
+++ wisejournal/posts/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Group
-from .models import Post
+
+from .models import Group, Post
 
 
 def index(request):
@@ -12,4 +12,3 @@ def group_posts(request, slug):
     group = get_object_or_404(Group, slug=slug)
     posts = Post.objects.filter(group=group).order_by('-pub_date')[:12]
     return render(request, 'group.html', {'group': group, 'posts': posts})
-
--- wisejournal/wisejournal/settings.py
+++ wisejournal/wisejournal/settings.py
@@ -79,6 +79,9 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+
+                # CUSTOM CONTEXT PROCESSORS
+                'posts.context_processors.year',
             ],
         },
     },
